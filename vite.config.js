ea1473782cda76ab9649de28e1a1437803628742
MarcusEscalona <152345428+MarcusEscalona@users.noreply.github.com>
2026-03-27 10:14:29 -0600

    Fix: Ruta absoluta estricta para el favicon

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cit-sync/', // <-- AGREGA ESTA LÍNEA (con el nombre de tu repo rodeado de diagonales)
+  base: '/cit-sync/',
 })
